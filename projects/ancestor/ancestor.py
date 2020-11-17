da432740661d92a88d2659ef101d7a58f646b2ce
Robert Kulp <pedalinon@gmail.com>
2020-11-16 20:41:31 -0800

    spittint out the adjacency list from the list

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,33 +1,7 @@
 from util import Stack, Queue
 from collections import deque
-
-# children = {
-# 	1: {3},
-# 	2: {3},
-# 	3: {6},
-# 	4: {5, 8},
-# 	5: {6, 7},
-# 	6: {},
-# 	7: {},
-# 	8: {9},
-# 	9: {},
-# 	10: {1},
-# 	11: {8}
-# }
-
-# ancestors = {
-# 	11: {},
-# 	10: {},
-# 	9: {8},
-# 	8: {4, 11},
-# 	7: {5},
-# 	6: {3, 5},
-# 	5: {4},
-# 	4: {},
-# 	3: {1, 2},
-# 	2: {},
-# 	1: {10}
-# }
+from graph import Graph
+from collections import defaultdict	
 
 ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
@@ -35,10 +9,20 @@ ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11
 def earliest_ancestor(ancestors, starting_node):
 	queue = deque()
 	queue.append(starting_node)
+	relationship = {}
+	temp = {}
 	for ancestor in ancestors:
-		parent = ancestor[0]
-		child = ancestor[1]
-		# print(parent, child)
+		if ancestor[0] in relationship:
+			# print('In relationship')
+			relationship[ancestor[0]] = {temp, ancestor[1]}
+			temp = ancestor[1]
+			# print('Relationship', relationship)
+		else:
+			# print('Not in relationship')
+			temp = ancestor[1]
+			relationship[ancestor[0]] = {ancestor[1]}
+		
+	print(relationship)
 
 
 	
@@ -46,9 +30,8 @@ def earliest_ancestor(ancestors, starting_node):
 
 # print('Targeted', ancestors[8])
 print('')
-earliest_ancestor(ancestors, 11)
+print('Ancestor', earliest_ancestor(ancestors, 9))
 print('')
 # Earliest known ancestor will have the longest path
 # If more than one ancestor, return the one with the lowest numberic ID/value.
 # Return -1 if there no parents exist. 
-
